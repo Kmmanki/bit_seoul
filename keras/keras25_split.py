9ced8c38d6a2fd2b2b844016fc56c6475c1903dd
unknown <aksrl25@gmail.com>
2020-11-13 16:53:27 +0900

    데이터 슬라이싱

--- keras/keras25_split.py
+++ keras/keras25_split.py
@@ -1,31 +1,33 @@
 import numpy as np 
 
-dataset = np.array(range(1, 31))
-size= int(int(len(dataset)) /2)
+if __name__ == "__main__":
 
-def split_x(seq, size):
-    aaa = []
+    dataset = np.array(range(1, 31))
+    size= int(int(len(dataset)) /2)
 
-    # 11 - 5 =6 
-    for i in range(len(seq)-  size +1): # 행 만드는 반복문 
-        
-        subset =seq[i : (i+size)] #dataset 에서 한칸씩 밀어나면서 size 갯수 만큼 꺼내기
-        # [0 : 5] 1 2 3 4 5 
-        # [1 : 6] 2 3 4 5 6  
-        # [2 : 7] 3 4 5 6 7 
-        # [2 : 8] 4 5 6 7 8 
-        
+    def split_x(seq, size):
+        aaa = []
 
-        aaa.append([item for item in subset]) 
-        #append(1 2 3 4 5) i = 0
-        #append(2 3 4 5 6) i = 1
-        #append(3 4 5 6 7) i = 2 
-        #append(4 5 6 7 8) i = 3
-        #append(5 6 7 8 9) i = 4
-        #append(6 7 8 9 10) i = 5
-    print(type(aaa))
-    return np.array(aaa)
+        # 11 - 5 =6 
+        for i in range(len(seq)-  size +1): # 행 만드는 반복문 
+            
+            subset =seq[i : (i+size)] #dataset 에서 한칸씩 밀어나면서 size 갯수 만큼 꺼내기
+            # [0 : 5] 1 2 3 4 5 
+            # [1 : 6] 2 3 4 5 6  
+            # [2 : 7] 3 4 5 6 7 
+            # [2 : 8] 4 5 6 7 8 
+            
 
-datasets = split_x(dataset, size)
-print("===========================================================")
-print(datasets)
+            aaa.append([item for item in subset])  #->([subset])
+            #append(1 2 3 4 5) i = 0
+            #append(2 3 4 5 6) i = 1
+            #append(3 4 5 6 7) i = 2 
+            #append(4 5 6 7 8) i = 3
+            #append(5 6 7 8 9) i = 4
+            #append(6 7 8 9 10) i = 5
+        print(type(aaa))
+        return np.array(aaa)
+
+    datasets = split_x(dataset, size)
+    print("===========================================================")
+    print(datasets)
