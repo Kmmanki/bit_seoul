452d1b6b7714382383deddb917cf22aebb8248f0
unknown <aksrl25@gmail.com>
2020-11-19 18:20:47 +0900

    주가 자르기

---
+++ keras/keras62_split2.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+data = []
+for i in range(100):
+    day = []
+    day.append(i)
+    day.append(i)
+    day.append(i)
+    day.append(i)
+    data.append(day)
+
+data = np.array(data)
+# print(data)
+
+# print(data)
+
+
+
+
+def spli(data, size):
+    result = [] 
+    for i in range(data.shape[0]):
+        if i+ 5 <  data.shape[0]:
+            result.append(data[i: i+size])
+        else:
+            return np.array(result)
+    return np.array(result)
+
+
+tmp = spli(data, 5)
+print(tmp)
+print(tmp.shape)
