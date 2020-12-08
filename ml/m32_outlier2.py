a7e2cb5de7670f002923bdfe5294744e3d1b0edb
unknown <aksrl25@gmail.com>
2020-12-08 09:17:39 +0900

    outlier

--- ml/m32_outlier2.py
+++ ml/m32_outlier2.py
@@ -1,25 +1,19 @@
 import numpy as np
 
-a = np.array(
-    [[1,2,3,4, 10000, 6,7,5000,90,100],
-    [10000,20000,-80000,40000,50000,60000,70000,8,90000,100000],
-    ])
-
 def outliers(data_out) :
-    data_out = data_out.T # 뒤집어서 빼기 편하게
-    del_index = np.array([],dtype=np.int)
-    for cnt in range(data_out.shape[1]):
-        column_data = data_out[:,cnt]
-        quartile_1, quartile_3 = np.percentile(column_data, [25,75]) #데이터의 25%, 75%지점
-        iqr = quartile_3 - quartile_1
+    list=[]
+    for i in range(len(data_out)) :
+        quartile_1, quartile_3=np.percentile(data_out[i], [25, 75]) 
+        print(i,"번째 인덱스의 1사분위 :", quartile_1) 
+        print(i,"번째 인덱스의 3사분위 :", quartile_3) 
+        iqr = quartile_3 - quartile_1 
         lower_bound = quartile_1 - (iqr * 1.5)
         upper_bound = quartile_3 + (iqr * 1.5)
-        outlier_index = np.where((column_data>upper_bound)|(column_data<lower_bound))
-        del_index = np.append(del_index, outlier_index) # 인덱스를 모아서
-    data_out = np.delete(data_out, del_index, axis=0) # 모은 인덱스를 빼라
-    data_out = data_out.T # 다시 뒤집자
-    return data_out
+        list.append(np.where(([data_out[i]]>upper_bound) | (data_out[i]<lower_bound)))
 
+    return list
+
+a = np.array([[1,2,3,4,10000], [6, 7,5000, 90, 100]])
 
 b = outliers(a)
 print("이상치의 위치 : ", b) 
